9b42818317b2d28c6245b05c584d9bd17a68cd33
Federico Peirano <fedepr2345@gmail.com>
2024-03-18 16:45:25 -0300

    a. Flux-architecture y Redux - Ejercicios6.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+import { voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -9,15 +10,6 @@ const App = () => {
     console.log('Voting...', id)
     console.log('------------------------')
     dispatch(voteAnecdote(id))
-  }
-
-  const newAnecdote = (event) => {
-    event.preventDefault()
-    console.log('Creating anecdote...', event.target.anecdote)
-    console.log('------------------------')
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
   }
 
   return (
@@ -35,10 +27,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form onSubmit={newAnecdote}>
-        <div><input name='anecdote'/></div>
-        <button type="submit">create</button>
-      </form>
+        <AnecdoteForm/>
     </div>
   )
 }
