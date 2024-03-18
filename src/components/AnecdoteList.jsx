2b27ca9efdb77629b8fc8348d85e5ee1c25b542d
Federico Peirano <fedepr2345@gmail.com>
2024-03-18 18:24:36 -0300

    Filter Funcionando

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
+
+  const [filterContent, setFilterContent] = useState('');
+
+  const filteredAnecdotes = anecdotes.filter(anecdote =>
+     anecdote.content.toLowerCase().includes(filterContent.toLowerCase())
+  );
   
   const vote = (id) => {
     console.log('Voting...', id)
@@ -12,19 +19,27 @@ const AnecdoteList = () => {
   }
 
   return(
-    <ul>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-      <div key={anecdote.id}>
-        <div>
-          {anecdote.content}
+    <div>
+      <input
+        type="text"
+        placeholder="Search anecdotes..."
+        value={filterContent}
+        onChange={(event) => setFilterContent(event.target.value)}
+      />
+      <ul>
+      {filteredAnecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+        <div key={anecdote.id}>
+            <div>
+            {anecdote.content}
+            </div>
+            <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
         </div>
-        <div>
-          has {anecdote.votes}
-          <button onClick={() => vote(anecdote.id)}>vote</button>
-        </div>
-      </div>
-      )}
-    </ul>
+        )}
+      </ul>
+    </div>
   )
 }
 
