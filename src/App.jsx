0e74817d651b6b5a0b6f0a1cf252039fdc509dd6
Federico Peirano <fedepr2345@gmail.com>
2024-03-19 13:06:36 -0300

    c. Comunicarse con el Servidor en una Aplicacion Redux - Ejercicios6.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import VisibilityFilter from './components/VisibilityFilter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdote } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdote(anecdotes)))
+  }, [])
+
   return (
     <div>
       <Notification/>
